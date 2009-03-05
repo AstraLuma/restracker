4e102193c8ea64dbc2491dfb227fe8dcfb71aa90
James Bliss <james.bliss@astro73.com>
2009-03-05 01:02:42 -0500

    Bad docstring.

--- restrack/pages/reservations.py
+++ restrack/pages/reservations.py
@@ -1,7 +1,7 @@
 # -*- tab-width: 4; use-tabs: 1; coding: utf-8 -*-
 # vim:tabstop=4:noexpandtab:
 """
-Stuff dealing with rooms.
+Stuff dealing with reservations.
 """
 from restrack.web import page, template, HTTPError
 from restrack.utils import struct, result2obj, first
